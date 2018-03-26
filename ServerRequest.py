f5106b17c423b9997579794d04cd12dabf277495
SopTom <2104soso@gmail.com>
2018-03-26 16:21:37 +0200

    Added until TimeSeriesData

--- ServerRequest.py
+++ ServerRequest.py
@@ -15,27 +15,104 @@ def queryTheUsersCustomerList():
 def addNewCustomer():
     suffix  = '/1/customers'
     post = {
-  "name": "enter name",
-  "street": "enter street",
-  "city": "enter city",
-  "note": "enter note"
-}
+        "name": "enter name",
+        "street": "enter street",
+        "city": "enter city",
+        "note": "enter note"
+    }
     return requests.post(url + suffix, json=post, auth=HTTPBasicAuth(cid, passwd))
 
 def queryCustomersProfile():
     suffix = '/1/customers' + cid
-    return requests.put(url + suffix, json=put, auth=HTTPBasicAuth(cid, passwd))
+    return requests.put(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
-def modifyCustomer
+def modifyCustomerProfile():
+    suffix = '/1/customer' + cid
+    put = {"street": "enter street",
+           "city": "enter city",
+           "note": "enter note"
+           }
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def sendEmailOnBehalfThisCustomer():
+    suffix = '/1/customer/' + cid + '/cn-sending/'
+    post = {"receivers": [
+    "person1@test.com",
+    "person2@test.com"
+  ],
+  "subject": "Hi all - from rapidM2M!",
+  "body": "<html><h1>Hi all</h1>My first message sent via rapidM2M BACKEND API</html>"
+}
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
 
 def modifyCustomerProfile():
     suffix = "/1/customers/" + cid
     data = {
-      "street": "enter street",
-      "city": "enter city",
-      "note": "enter note"
+        "street": "enter street",
+        "city": "enter city",
+        "note": "enter note"
     }
-    return requests.put(url + suffix, json=data, auth=HTTPBasicAuth(cid, passwd))
+    return requests.get(url + suffix, json=data, auth=HTTPBasicAuth(cid, passwd))
+
+def deleteCustomerAllHisSitesAndData():
+    suffix = '/1/customer'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def sendSMSBehalfThisCostumer():
+    suffix = '/1/costumer' + cid + '/cn-sendsms/'
+    post = {"receivers": [
+    "+4311111111",
+    "+4312222222"
+  ],
+  "subject": "Hi all - from rapidM2M!"
+}
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+#Manage a site
+
+def queryCostomersListOfSites():
+    suffix = '/1/customer' + cid + '/sites/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def querySitesBlueprint():
+    suffix = '/1/customer' +cid + '/sites/' + sid + '/blueprint/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+
+#Manage a divice
+
+#Sites status&configuration data
+
+def querySitesConfigurtion():
+    suffix = '/1/costumer' + cid + '/sites/' + sid + '/config0/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+
+def modifySitesConfiguration():
+    suffix = '/1/customers/' + cid + '/config0/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+
+#SitesTimeSeriesData
+
+def queryYoungstRawValues():
+    suffix = '/1/customer' + cid + '/sites/' + sid + '/histdata0/youngest'
+    get = {
+  "select": [
+    "ch0",
+    "ch3",
+    "ch1"
+  ]
+}
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+
+
+
+
+
+
+
+
+
+
 
 
 
