210aee143005d8d71a34428a05e9250461fa1a25
SopTom <2104soso@gmail.com>
2018-03-26 13:55:51 +0200

    Working on ServerRequests

--- ServerRequest.py
+++ ServerRequest.py
@@ -4,15 +4,29 @@ from credential import *
 
 #The first thing upon user login
 def queryLoggedInUserAndCustomerList():
-    suffix = '/1/me' + cid
+    suffix = '/1/me'
     return requests.get(url+suffix,auth=HTTPBasicAuth(cid, passwd))
 
 #Manage acustomer
 def queryTheUsersCustomerList():
-    suffix = '/1/customers' + cid
+    suffix = '/1/customers'
     return requests.get(url+suffix,auth=HTTPBasicAuth(cid, passwd))
 
+def addNewCustomer():
+    suffix  = '/1/customers'
+    post = {
+  "name": "enter name",
+  "street": "enter street",
+  "city": "enter city",
+  "note": "enter note"
+}
+    return requests.post(url + suffix, json=post, auth=HTTPBasicAuth(cid, passwd))
 
+def queryCustomersProfile():
+    suffix = '/1/customers' + cid
+    return requests.put(url + suffix, json=put, auth=HTTPBasicAuth(cid, passwd))
+
+def modifyCustomer
 
 def modifyCustomerProfile():
     suffix = "/1/customers/" + cid
