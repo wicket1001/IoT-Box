e6b643efdbbbf8cd99140a5ecae676e7222c0e0e
SopTom <2104soso@gmail.com>
2018-03-26 13:33:30 +0200

    Added first 2 requests.

--- ServerRequest.py
+++ ServerRequest.py
@@ -2,6 +2,17 @@ import requests
 from requests.auth import HTTPBasicAuth
 from credential import *
 
+#The first thing upon user login
+def queryLoggedInUserAndCustomerList():
+    suffix = '/1/me' + cid
+    return requests.get(url+suffix,auth=HTTPBasicAuth(cid, passwd))
+
+#Manage acustomer
+def queryTheUsersCustomerList():
+    suffix = '/1/customers' + cid
+    return requests.get(url+suffix,auth=HTTPBasicAuth(cid, passwd))
+
+
 
 def modifyCustomerProfile():
     suffix = "/1/customers/" + cid
