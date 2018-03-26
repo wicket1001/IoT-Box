6a8e487cc2478260c6ddaa4dfad18134489d88d5
SopTom <2104soso@gmail.com>
2018-03-26 16:55:57 +0200

    Finished relevant parts of Server requests

--- ServerRequest.py
+++ ServerRequest.py
@@ -37,12 +37,12 @@ def modifyCustomerProfile():
 def sendEmailOnBehalfThisCustomer():
     suffix = '/1/customer/' + cid + '/cn-sending/'
     post = {"receivers": [
-    "person1@test.com",
-    "person2@test.com"
-  ],
-  "subject": "Hi all - from rapidM2M!",
-  "body": "<html><h1>Hi all</h1>My first message sent via rapidM2M BACKEND API</html>"
-}
+        "person1@test.com",
+        "person2@test.com"
+    ],
+        "subject": "Hi all - from rapidM2M!",
+        "body": "<html><h1>Hi all</h1>My first message sent via rapidM2M BACKEND API</html>"
+    }
     return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
 
@@ -62,11 +62,11 @@ def deleteCustomerAllHisSitesAndData():
 def sendSMSBehalfThisCostumer():
     suffix = '/1/costumer' + cid + '/cn-sendsms/'
     post = {"receivers": [
-    "+4311111111",
-    "+4312222222"
-  ],
-  "subject": "Hi all - from rapidM2M!"
-}
+        "+4311111111",
+        "+4312222222"
+    ],
+        "subject": "Hi all - from rapidM2M!"
+    }
     return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
 #Manage a site
@@ -77,7 +77,7 @@ def queryCostomersListOfSites():
 
 def querySitesBlueprint():
     suffix = '/1/customer' +cid + '/sites/' + sid + '/blueprint/'
-    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
 #Manage a divice
 
@@ -85,24 +85,74 @@ def querySitesBlueprint():
 
 def querySitesConfigurtion():
     suffix = '/1/costumer' + cid + '/sites/' + sid + '/config0/'
-    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
 def modifySitesConfiguration():
     suffix = '/1/customers/' + cid + '/config0/'
-    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
 
 #SitesTimeSeriesData
 
 def queryYoungstRawValues():
     suffix = '/1/customer' + cid + '/sites/' + sid + '/histdata0/youngest'
     get = {
-  "select": [
-    "ch0",
-    "ch3",
-    "ch1"
-  ]
+        "select": [
+            "ch0",
+            "ch3",
+            "ch1"
+        ]
+    }
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def queryTimeWindowWithRawValues():
+    suffix = '/1/costomers' + cid + '/sites/' + sid + '/histdata0'
+    get = { "select": [
+        "ch0",
+        "ch3",
+        "ch1"
+    ],
+        "from": "20150101",
+        "until": "*"
+    }
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def instertNewValuesHistorialDataChannel():
+    suffix = '/1/customers' + cid + '/siter/' + sid +'/histdata0'
+    post = {
+  "stamp": "201501011030",
+  "ch0": 1,
+  "myFieldName": 3.1,
+  "textField": "demo_text"
 }
-    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd)
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+#Sites position data
+
+def queryyoungestpositionValues():
+    suffix = '/1/customer' + cid + '/sites/' + sid + '/pos/youngest/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+#Manage users
+
+def queryUserList():
+    suffix = '/1/users'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def queryCustomersUserList():
+    suffix = '/1/customer' + cid + '/users/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+def queryUsersProfile():
+    suffix = '/1/custeromers/' + cid + '/users/'
+    return requests.get(url + suffix, auth=HTTPBasicAuth(cid, passwd))
+
+
+
+
+
+
+
+
 
 
 
